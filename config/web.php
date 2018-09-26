35e14b9d06b9a67284af979854d2e3701d2e6bb1
khuc ngoc dat <datkhucngoc@admicro.vn>
2018-09-26 17:10:43 +0700

    get started - tao module test - khai bao module trong file config web.php - tao CRUD cho bang country

--- config/web.php
+++ config/web.php
@@ -43,6 +43,7 @@ $config = [
             ],
         ],
         'db' => $db,
+
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
@@ -53,6 +54,11 @@ $config = [
         */
     ],
     'params' => $params,
+    'modules' => [
+        'test' => [
+            'class' => 'app\modules\test\Module',
+        ]
+    ],
 ];
 
 if (YII_ENV_DEV) {
